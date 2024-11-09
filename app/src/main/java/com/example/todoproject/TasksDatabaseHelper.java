ebfaa2d7b1969a42e79952749945025543d47c89
dyanesh003 <dyanesh003@gmail.com>
2024-11-09 10:15:55 +0530

    10:15 am commit

--- app/src/main/java/com/example/todoproject/TasksDatabaseHelper.java
+++ app/src/main/java/com/example/todoproject/TasksDatabaseHelper.java
@@ -5,16 +5,8 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class TasksDatabaseHelper extends SQLiteOpenHelper {
 
